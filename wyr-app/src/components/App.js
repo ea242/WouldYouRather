2af5f0fcfafcac58f888388eee7d2d0f68093481
Eric Atempa <ea242@njit.edu>
2020-02-28 19:12:21 -0500

    setting up the api actions reducers and middleware

---
+++ wyr-app/src/components/App.js
@@ -0,0 +1,29 @@
+import React, { Component, Fragment } from 'react'
+// import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import LoadingBar from 'react-redux-loading'
+// import Nav from './Nav'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+  render() {
+    return(
+      <div className="App">
+        <LoadingBar />
+        hi
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({authedUser}) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
